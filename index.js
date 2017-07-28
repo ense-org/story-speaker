0de458f18364d78538c0464a1145f06d12d3e29a
Clyde Shaffer <clydeshaffer@gmail.com>
2017-07-28 12:49:17 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,111 @@
+var fs = require('fs');
+var https = require('https');
+
+var childproc = require('child_process');
+
+var targetStory = 'speakerTest';
+
+var startupDate = new Date();
+
+var ensePlaying = false;
+
+var enseQueue = [];
+var highestIdQueued = 0;
+
+var pendingCheck = false;
+
+var fileNum = 0;
+
+var filenamePlayNext = null;
+
+var pendingDownload = false;
+
+function err(e) { console.error("Error:", e);}
+
+var download = function(url, dest, cb) {
+  var file = fs.createWriteStream(dest);
+  var request = https.get(url, function(response) {
+    response.pipe(file);
+    file.on('finish', function() {
+      file.close(cb);  // close() is async, call cb after close completes.
+    });
+  }).on('error', function(err) { // Handle errors
+    fs.unlink(dest); // Delete the file async. (But we don't check the result)
+    if (cb) cb(err.message);
+  });
+};
+
+function playAudio(filename, cb) {
+        ensePlaying = true;
+        childproc.spawn('mplayer' , [ filename]).on('close', () => { ensePlaying                                                                                                                                    = false; cb();});
+}
+
+function playNextLoop() {
+        playAudio(filenamePlayNext, playNextLoop);
+        filenamePlayNext = null;
+}
+
+function checkQueue() {
+        var nextEnse = enseQueue.pop();
+        if(nextEnse) {
+                var destFile = (fileNum++) + ".m4a";
+                fileNum = fileNum % 4;
+                var audUrl = nextEnse[1].fileUrl;
+                pendingDownload = true;
+                console.log("DOWNLOADING", nextEnse[0]);
+                download(audUrl, destFile, (e) => {
+                        if(!e) {
+                                filenamePlayNext = destFile;
+                                if(!ensePlaying) playNextLoop();
+                        } else { err(e); }
+                        pendingDownload = false;
+                });
+        }
+}
+
+function addToQueue(list) {
+        var idBookmark = highestIdQueued;
+        var highestQueuedNow = idBookmark;
+        list.forEach((item) => {
+                var itemId = parseInt(item[0]);
+                var parsedDate = new Date(item[1].timestamp);
+                if(itemId > idBookmark && parsedDate > startupDate) {
+                        console.log("ENQUEUE", itemId);
+                        enseQueue.push(item);
+                        if(itemId > highestQueuedNow) highestQueuedNow = itemId;
+                }
+        });
+        highestIdQueued = highestQueuedNow;
+}
+
+function checkForEnses() {
+        pendingCheck = true;
+        var recievedData = "";
+        https.get('https://api.ense.nyc/topics/' + targetStory, (res) => {
+                pendingCheck = false;
+                res.on('data', (d) => {
+                        recievedData += d;
+                });
+                res.on('end', () => {
+                        var resp = JSON.parse(recievedData);
+                        addToQueue(resp.enses);
+
+                        checkQueue();
+                });
+        }).on('error', (e) => {
+                err(e);
+                pendingCheck = false;
+        });
+}
+
+function mainCycle() {
+        checkForEnses();
+        if(!filenamePlayNext && !pendingDownload) {
+                checkQueue();
+        }
+}
+
+setInterval(mainCycle, 3000);
+
+filenamePlayNext = "online.m4a";
+playNextLoop();
