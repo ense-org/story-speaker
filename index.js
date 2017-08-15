59c9022946ae991e03ac73e63df43d1f90afb393
Clyde Shaffer <clydeshaffer@gmail.com>
2017-08-15 15:49:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function mainCycle() {
 }
 
 function doReboot() {
-  require('child_process').exec('sudo /sbin/shutdown -r now', function (msg) { console.log(msg) });
+  //require('child_process').exec('sudo /sbin/shutdown -r now', function (msg) { console.log(msg) });
 }
 
 function updateCycle() {
